cbffcd46eaef1526611092f0ed55651d28edc67d
pierre.delaunay <delaunap@rtx5.server.mila.quebec>
2026-03-20 15:15:10 -0400

    PIN torch2.10+cu13

--- milabench/_version.py
+++ milabench/_version.py
@@ -1,5 +1,5 @@
 """This file is generated, do not modify"""
 
-__tag__ = "v1.2.2-106-g540b30b"
-__commit__ = "540b30b66f029e33563c8c6047f8187b78bd5263"
-__date__ = "2026-03-10 14:59:10 +0000"
+__tag__ = "v1.2.2-129-g36521a7"
+__commit__ = "36521a71b62b8e84b9da8a1c290fecbdf5e5bcc9"
+__date__ = "2026-03-20 18:19:15 +0000"
