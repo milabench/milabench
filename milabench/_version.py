70c6951e633219f1e15ccd713f413e3c3231912b
Setepenre <pierre.delaunay.tr@gmail.com>
2026-07-29 11:37:55 -0400

    New dependencies (#28)
    * New dependency management
    
    * Update Timeline sampling for inference benchmarks

--- milabench/_version.py
+++ milabench/_version.py
@@ -1,8 +1,8 @@
 """This file is generated, do not modify"""
 
-__tag__ = "v1.3.2-26-gf5a8027"
-__commit__ = "f5a80277efb97c07e40ad5c3a1045aab94601a76"
-__date__ = "2026-06-08 17:53:54 -0400"
+__tag__ = "v1.3.2-36-g521df52"
+__commit__ = "521df521a7d07ac21adfe1d5bb153c86067e2abd"
+__date__ = "2026-06-30 11:12:46 -0400"
 __branch__ = "None"
 __repo__ = "None"
 __pr_url__ = "None"
