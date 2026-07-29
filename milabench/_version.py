b8615f57e3c19ece7f0cc4a3fcd5df77d0423da3
pierre.delaunay <delaunap@rtx5.server.mila.quebec>
2026-07-29 12:54:05 -0400

    Pin Dependencies

--- milabench/_version.py
+++ milabench/_version.py
@@ -1,8 +1,8 @@
 """This file is generated, do not modify"""
 
-__tag__ = "v1.3.2-37-g24dd484"
-__commit__ = "24dd48463e96420b4198736e70533a22aa1f3804"
-__date__ = "2026-07-27 12:56:22 -0400"
+__tag__ = "v1.3.2-45-g8603adc"
+__commit__ = "8603adce20e29d99d4fa4e7af4523fe9e191e320"
+__date__ = "2026-07-29 11:43:57 -0400"
 __branch__ = "None"
 __repo__ = "None"
 __pr_url__ = "None"
