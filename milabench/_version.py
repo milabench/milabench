8603adce20e29d99d4fa4e7af4523fe9e191e320
Pierre Delaunay <pierre.delaunay@outlook.com>
2026-07-29 11:42:52 -0400

    Add torchatari config back to training

--- milabench/_version.py
+++ milabench/_version.py
@@ -1,14 +1,8 @@
 """This file is generated, do not modify"""
 
-<<<<<<< HEAD
-__tag__ = "v1.3.2-36-g521df52"
-__commit__ = "521df521a7d07ac21adfe1d5bb153c86067e2abd"
-__date__ = "2026-06-30 11:12:46 -0400"
-=======
 __tag__ = "v1.3.2-37-g24dd484"
 __commit__ = "24dd48463e96420b4198736e70533a22aa1f3804"
 __date__ = "2026-07-27 12:56:22 -0400"
->>>>>>> 4ee15fb7f38892c35325367eaafa480b52837051
 __branch__ = "None"
 __repo__ = "None"
 __pr_url__ = "None"
